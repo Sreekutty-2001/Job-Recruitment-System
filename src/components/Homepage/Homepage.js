05430cfbf44647393471f8edc6f631e4c8fc2394
Sreekutty <sreekuttisreekutti35@gmail.com>
2023-04-02 20:11:44 +0530

    Finised Homepage

---
+++ src/components/Homepage/Homepage.js
@@ -0,0 +1,31 @@
+import React from 'react'
+import './Homepage.css'
+function Homepage() {
+  return (
+    <div>
+      <div className="image">
+      <h2 className='writting-one'>WE CREATED STEPPING STONES FOR YOUR </h2>
+      
+      <h1 className='writting-two'>SUCCESS</h1>
+      
+     
+       <button  className='search'>Search</button>
+       
+      </div>
+      
+      
+      
+      
+    
+          
+        
+        
+      
+        
+
+     
+    </div>
+  )
+}
+
+export default Homepage
